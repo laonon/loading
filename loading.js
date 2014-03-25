4972e4f0b3e358fecee654e73d8709f1b9564b20
laonon <laonon2012@gmail.com>
2014-03-26 00:47:25 +0800

    增加失败状态

--- loading.js
+++ loading.js
@@ -1,4 +1,4 @@
-(function(window){
+(function(window) {
     var Resource = {
         images: {
             //path：文件相对路径，size:图片的大小，data：存放加载的图片数据。
@@ -133,32 +133,29 @@
 
     Progress.PreLoadData = function() {
         var loadedNum = 0, //已加载资源数量
-            resourceNum = 0,
             resourceNum1 = 15, //图片资源数量
             resourceNum2 = 9; //音频资源数量
-        var handleSucc = function() {//资源加载完成后的回调函数
-        }; 
+        var handleSucc = function() { //资源加载完成后的回调函数
+
+        };
+        var handleFail = function() { //资源加载失败的回调函数
+
+        };
 
         function imageLoadSucc() { //每成功加载一个图片执行一次
             loadedNum++;
-            var cur = (loadedNum / 24).toFixed(2) * 100 + '%';
-            percent.innerText = cur;
             if (loadedNum === resourceNum1) { //全部图片文件加载完后，继续加载声音
-                console.log('开始加载音频');
                 loadSound();
             }
         }
 
         function soundLoadSucc() { //每成功加载一个声音执行一次
             loadedNum++;
-            var cur = (loadedNum / 24).toFixed(2) * 100 + '%';
-            percent.innerText = cur;
-            if (loadedNum === 24) { //全部声音文件加载完后，执行回调函数
-                percent.innerText = '100%';
+            if (loadedNum === (resourceNum1 + resourceNum2)) { //全部声音文件加载完后，执行回调函数
                 handleSucc();
             }
         }
-        
+
         function loadImage() { //加载图片
             for (var i in Resource['images']) {
                 Resource['images'][i]['data'] = new Image();
@@ -167,8 +164,7 @@
                     imageLoadSucc();
                 }
                 Resource['images'][i]['data'].onerror = function() {
-                    console.log("资源加载失败！")
-                    return;
+                    handleFail();
                 }
             }
         }
@@ -182,26 +178,29 @@
                 } else {
                     Resource['sounds'][j]['data'].src = Resource['sounds'][j]['path'] + Resource['sounds'][j]['soundName'] + ".mp3";
                 }
-                Resource['sounds'][j]['data'].addEventListener('canplaythrough', function() {
+                Resource['sounds'][j]['data'].addEventListener('canplaythrough', function() { //ios下safria会呈挂起状态
                     soundLoadSucc();
                 }, false);
                 Resource['sounds'][j]['data'].addEventListener("error", function() {
-                    console.log("资源加载失败！")
-                    return;
+                    handleFail();
                 }, false);
             }
         }
-
-       
         loadImage();
-        return {
-            done: function(f) {
-                if (f) {
-                    handleSucc = f;
+        return { //返回一个对象
+            succ: function(f1) {
+                if (f1) {
+                    handleSucc = f1;
+                }
+            },
+            fail: function(f2) {
+                if (f2) {
+                    handleFail = f2;
                 }
             }
+
         }
     };
-    
+
     window.Progress = Progress;
 })(window);
